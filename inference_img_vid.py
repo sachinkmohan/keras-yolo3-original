01de6a1d3a8dc68c1b075964d2d3de387e53038c
sachinkm308 <sachinkm308@gmail.com>
2022-04-28 17:54:05 +0200

    pruning initial, inference custom

---
+++ inference_img_vid.py
@@ -0,0 +1,8 @@
+from yolo_custom2 import *
+
+obj = YOLO()
+#b = obj.detect_image('./additional_custom/street.jpg')
+c =obj.detect_image('./additional_custom/random_car.jpg')
+
+
+#detect_image('./additional_custom/street.jpg')
